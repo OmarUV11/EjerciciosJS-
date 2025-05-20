a909dce94fdb4650c390f9295da5085ae1c78a79
Omar David Conde Suarez <103803283+OmarUV11@users.noreply.github.com>
2025-05-20 13:32:40 -0600

    Ejercicios Basicos JavaScript
    Ejercicios para mejorar la logia de programación de nivel básico hasta dominar el DOM

---
+++ JS-Basicos/SumarDosNumeros.js
@@ -0,0 +1,8 @@
+function sumarNumeros(){
+    let numero1 = parseInt(prompt("Digite el primer numero:"));
+    let numero2 = parseInt(prompt("Digite el segundo numero:"));
+    let resultado = numero1 + numero2;
+
+    return alert(`La suma es: ${resultado}`);
+}
+sumarNumeros();
